ccb6f4092e903129689743b7904af5397f4339b1
George <30464774+Getos@users.noreply.github.com>
2024-06-16 01:56:46 +0300

    added game score capped at 5 then reset

--- RPS.js
+++ RPS.js
@@ -1,18 +1,33 @@
-//selecting the id of the buttons
+//intialalization of selectors
 let humanChoice;
 const rockBtn = document.querySelector("#rock")
 const paperBtn = document.querySelector("#paper")
 const scissorsBtn = document.querySelector("#scissors")
-
-let humanScore = 0;
-let computerScore = 0;
-function displayScore() {
-    const container = document.querySelector(".results")
+const container = document.querySelector(".results")
 const content = document.createElement("div");
 content.classList.add("content");
-content.textContent = "your score is " + humanScore + " \r\n java score is " + computerScore;
+let humanScore = 0;
+let computerScore = 0;
 container.appendChild(content);
+// function for the score display
+function displayScore() {
+content.textContent = `Your score is ${humanScore} \n \r Java score is ${computerScore}`
+// content.textContent = "your score is " + humanScore + " \u2028java score is " + computerScore;
+    if (humanScore === 5) {
+        content.textContent = "your score is  " + humanScore + "  you WON THE GAME"
+        humanScore = 0;
+        computerScore = 0 
+    }
+    else if (computerScore === 5)
+        {
+            content.textContent = "java score is  " + computerScore + "  you LOST THE GAME"
+            humanScore = 0;
+            computerScore = 0 
+        }
+
 }
+
+
 function getComputerChoice() {
     let randomNumber = 3 * Math.random()
     if (randomNumber <= 1) {
@@ -24,77 +39,37 @@ function getComputerChoice() {
         return "scissors"
     }
 }
-function getHumanChoice() {
-    while (true) {
-        humanChoice = prompt("Enter your choice (rock, paper, scissors):").trim().toLowerCase();
-        switch (humanChoice) {
-            case "rock":
-            case "paper":
-            case "scissors":
-                return humanChoice;
-            default:
-                alert("Not a valid option! Please choose rock, paper, or scissors.");
-        }
-    }
-}
+
 function playRound(humanChoice, computerChoice) {
 
         if (humanChoice === computerChoice) {
-            console.log("it's a tie")
         }
         else if (humanChoice === "rock" && computerChoice === "scissors") {
-            console.log(`you win ${humanChoice} beats ${computerChoice}`)
             humanScore++;
         }
         else if (humanChoice === "scissors" && computerChoice === "paper") {
-            console.log(`you win ${humanChoice} beats ${computerChoice}`)
             humanScore++;
         }
         else if (humanChoice === "paper'" && computerChoice === "rock") {
-            console.log(`you win ${humanChoice} beats ${computerChoice}`)
             humanScore++;
         }
         else {
-            console.log(`you lose ${computerChoice} beats ${humanChoice}`)
+           
             computerScore++;
     }
-    console.log(`your score is ${humanScore}`)
-    console.log(`java score is ${computerScore}`)
-    console.log(`your choice was ${humanChoice}`)
-    console.log(`java choice was ${computerChoice}`)
+
 }
 
 rockBtn.addEventListener("click", () => {
     playRound("rock",getComputerChoice())
-    alert("you choose rock")
     displayScore()
 })
 paperBtn.addEventListener("click", () => {
     playRound("paper",getComputerChoice())
-    alert("you choose paper")
     displayScore()
 })
 scissorsBtn.addEventListener("click", () => {
     playRound("scissors",getComputerChoice())
-    alert("you choose scissors")
     displayScore()
 })
-
-// function playGame() {
-//     for (let i = 0; i < 5; i++) {
-//         const humanSelection = getHumanChoice();
-//         const computerSelection = getComputerChoice();
-//         playRound(humanSelection, computerSelection);
-//     }
-//     if (humanScore > computerScore) {
-//         console.log("you win")
-//     }
-//     else if (humanScore < computerScore) {
-//         console.log("you lose")
-//     }
-//     else {
-//         console.log("tie")
-//     }
-// }
-// playGame()
 
